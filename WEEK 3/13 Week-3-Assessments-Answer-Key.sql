daa4aa4502382563242bf8a9eecd20411d2ff4ff
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2020-12-09 18:02:26 -0500

    Create 13 Week-3-Assessments-Answer-Key.sql

---
+++ WEEK 3/13 Week-3-Assessments-Answer-Key.sql
@@ -0,0 +1,33 @@
+# Question 2. On what day was Dillard’s	income based on	total	sum	of purchases the greatest?
+
+# Question 3. What is the	deptdesc of the	departments that have the top	3	greatest numbers of	skus from	the	skstinfo table associated	with them?
+
+# Question 4. Which	table	contains the most distinct sku numbers?
+
+# Question 5. How	many skus	are	in the skstinfo table, but NOT in	the skuinfo	table?
+
+# Question 6. What is	the	average	amount of	profit Dillard’s made	per	day?
+
+# Question 7. The store_msa table provides population statistics about the geographic location around a store.
+#             Using one query to retrieve your answer, how many MSAs are there within the state of North Carolina
+#             (abbreviated “NC”), and within these MSAs, what is the lowest population level (msa_pop) and highest income level (msa_income)?
+
+# Question 8. What department(with department	description), brand, style,	and	color	brought	in the greatest	total	amount of	sales?
+
+# Question 9. How	many stores	have more	than 180,000 distinct	skus associated	with them	in the skstinfo	table?
+
+# Question 10. Look at the data from all the distinct skus in the “cop” department with a “federal” brand and a “rinse wash” color. 
+#              You'll see that these skus have the same values in some of the columns, meaning that they have some features in common. 
+#              In which columns do these skus have different values from one another, meaning that their features differ in the categories 
+#              represented by the columns? Choose all that apply. Note that you will need more than a single correct selection to answer the question correctly.
+
+# Question 11. How many skus are in	the	skuinfo	table, but NOT in	the	skstinfo table?
+
+# Question 12. In what city and state is the store that had the greatest total sum of sales?
+
+# Question 13. Given Table A(first table to	be entered in	the	query) and Table B (second table to	be entered in	the	query)	
+#              the query result	shown	below	is a result	of what	kind of join?	
+
+# Question 14. How many	states have	more than	10 Dillards stores in	them?
+
+# Question 15. What	is the suggested retail	price	of all the skus	in the “reebok”	department with	the	“sketchers”	brand	and	a	“wht/saphire”	color?
